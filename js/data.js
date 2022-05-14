e64f57f65a779376029af900f68fc3508fa494d9
TanyaDanilovich <tan.danilovich@gmail.com>
2022-05-14 17:12:12 +0300

    commit before add slider

---
+++ js/data.js
@@ -0,0 +1,11 @@
+const user = [{
+	src: [
+		"./assets/img/857A1591-Edit_360x.webp",
+		"./assets/img/857A1584-2_360x.webp",
+		"./assets/img/857A1839_540x.webp",
+		"./assets/img/giftbox_675bf339-43f6-47ee-b9b9-806f01a05d16_360x.webp",
+		"./assets/img/ScreenShot2021-11-25at4.44.56PM_1c8ac9dd-9ab1-4cbe-a37a-8fb93b2c84f1_360x.webp"
+	]
+}]
+
+export { user }
--- js/sctipt.js
+++ js/sctipt.js
@@ -1,3 +1,6 @@
+import * as data from "./data.js"
+//console.dir(data.data[0].src[0]);
+
 (function () {
 	const HEADER = document.querySelector(".header__container");
 	const arrowUp = document.querySelector(".mobile-button-to-top");
@@ -17,6 +20,9 @@
 		}
 	}
 }());
+
+
+//Отображается доп изображение при наведении мышки в additional products
 
 const CARD_1 = document.querySelector('#additional-card-1')
 const IMG_1 = document.querySelector("#additional-image-12")
@@ -51,9 +57,28 @@ CARD_3.addEventListener('mouseout', () => {
 	IMG_3.classList.remove('background-additional-image-active')
 })
 
+//Загрузка главного изображения
+function loadMainImage(url) {
+	MAIN_IMG.src = `${url}`
+}
+//Загрузка изображений в слайдер
+function loadSmollSliderImage(url) {
+	SMOLL_SLIDER.forEach((img, index) => {
+		img.src = `${url[index]}`
+	})
+}
+
+const MAIN_IMG = document.getElementById('mainImg');
+const SMOLL_SLIDER = document.querySelectorAll(".smoll-slider-item");
 
 
 
 
+window.addEventListener("DOMContentLoaded", function () {
+	//loadMainImage("");
+	//loadSmollSliderImage()
 
-window.addEventListener("DOMContentLoaded", function () { console.log('страница загружена') })
+
+
+
+})
